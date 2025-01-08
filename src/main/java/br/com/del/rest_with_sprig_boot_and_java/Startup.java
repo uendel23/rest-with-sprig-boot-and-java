e57518576c33241726dbe10825fa84ee60799d4d
Uendel23 <carinha23@gmail.com>
2025-01-08 15:48:21 -0300

    primeiro commit java com spring

---
+++ src/main/java/br/com/del/rest_with_sprig_boot_and_java/Startup.java
@@ -0,0 +1,13 @@
+package br.com.del.rest_with_sprig_boot_and_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
